ff931d35328783bdec79e26e463d8b1ca4a928dc
TetianaChehrina <3909139@gmail.com>
2024-05-18 11:28:36 +0300

    task1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,33 @@
-// import { FiSearch } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+  const [values, setValues] = useState('');
+
+  const handleChange = e => {
+    setValues(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(values);
+  };
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        onChange={handleChange}
+        value={values}
+      />
+    </form>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,34 @@
 // import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Text, Form } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [photos, setPhotos] = useState('');
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [isError, setIsError] = useState(null);
+
+  const handleValues = value => {
+    setQuery(value);
+  };
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      try {
+        const response = getPhotos(query, page);
+        setPhotos(response.photos);
+      } catch (error) {
+        setIsError(error.message);
+      }
+    }
+    fetchPhotos();
+  }, [page, query]);
+
   return (
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
+      <Form onSubmit={handleValues} />
     </>
   );
 };
